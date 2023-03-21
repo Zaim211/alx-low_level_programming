b910e9df616e009217c6c894467b7d391ee4a3f8
Zaim211 <yousseftitazaim@gmail.com>
2023-03-21 10:56:58 -0700

    alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - writes lower case alphabet to stdo
+ *
+ *
+ * Return: void
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void print_alphabet(void)
+{
+	char ch[28] = "abcdefghijklmnopqrstuvwxyz\n";
+	int i = 0;
+
+	while (ch[i] != '\0')
+	{
+		_putchar(ch[i]);
+		i++;
+	}
+}
