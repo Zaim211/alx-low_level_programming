08bd59d4939bbf6116eaf682c6bb9c863f3ae0d9
Zaim211 <yousseftitazaim@gmail.com>
2023-03-29 15:43:54 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -13,8 +13,10 @@ char *_strncat(char *dest, char *src, int n)
 	int i;
 	int j;
 
-	for (i = 0; dest[i] != '\0'; i++)
+	i = 0;
+	for (dest[i] != '\0')
 	{
+		i++;
 	}
 	for (j < n && src[j] != '\0')
 	{
