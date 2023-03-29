88f0c962297c58ce82facc377da2ecba23091838
Zaim211 <yousseftitazaim@gmail.com>
2023-03-29 16:03:27 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,11 +11,13 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int j;
 
-	for (j = 0; j < n && src[j] != '\0'; j++)
+	j = 0;
+	while (j < n && src[j] != '\0')
 	{
 		dest[j] = src[j];
+		j++;
 	}
-	for (j < n)
+	while (j < n)
 	{
 		dest[j] = '\0';
 		j++;
