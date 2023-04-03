dcdd92299c3ba285152140bcd62b11502461f189
Zaim211 <yousseftitazaim@gmail.com>
2023-04-03 16:21:43 -0700

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -8,16 +8,17 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i, n, value, chack;
+	unsigned int i, n, value, check;
 
 	value = 0;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		check = 0;
+
 		for (n = 0; accept[n] != '\0'; n++)
 		{
-			if (s[i] == accept[n])
+			if (accept[n] == s[i])
 			{
 				value++;
 				check = 1;
