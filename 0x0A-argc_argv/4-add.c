3ae2a6c76f4bafd301fe92b2251b007d96ab0bcf
Zaim211 <yousseftitazaim@gmail.com>
2023-04-07 16:32:39 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -17,7 +17,7 @@ int main(int argc, char *argv[])
 		for (i = 0; i < argc; i++)
 		{
 			printf("s\n", argv[i]);
-			mul *= atoi(argv[i]);
+			sum *= atoi(argv[i]);
 		}
 		printf(Total = "%d\n", sum)
 	}
