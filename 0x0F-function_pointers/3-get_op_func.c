72a00eb5b653047d8cde66be5cc4b7aff9126f8c
Zaim211 <yousseftitazaim@gmail.com>
2023-04-19 18:24:33 -0700

    task 3

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -2,12 +2,12 @@
 #include <stdlib.h>
 /**
  * get_op_func - Selects the correct function to perform
- * the operation asked by the user.
+ *               the operation asked by the user.
  * @s: The operator passed as argument.
  *
  * Return: A pointer to the function corresponding
- * to the operator given as a parameter.
- **/
+ *         to the operator given as a parameter.
+ */
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
@@ -23,5 +23,6 @@ int (*get_op_func(char *s))(int, int)
 
 	while (ops[i].op != NULL && *(ops[i].op) != *s)
 		i++;
+
 	return (ops[i].f);
 }
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -30,7 +30,8 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 		exit(99);
 	}
 
-	if ((*op == '/' && num2 == 0) || (*op == '%' && num2 == 0))
+	if ((*op == '/' && num2 == 0) ||
+	    (*op == '%' && num2 == 0))
 	{
 		printf("Error\n");
 		exit(100);
@@ -38,6 +39,5 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 
 	printf("%d\n", get_op_func(op)(num1, num2));
 
-		return (0);
-	}
+	return (0);
 }
