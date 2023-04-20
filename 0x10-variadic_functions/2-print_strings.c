3fabcd34f47a26f781cb3bdbcd1e1e8e285ed65e
Zaim211 <yousseftitazaim@gmail.com>
2023-04-20 15:33:06 -0700

    2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -24,10 +24,11 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			printf("(nil)");
 		else
 			print("%s", str);
+
 		if (i != (n - 1) && separator != NULL)
 			printf("%s", separator)
 	}
 
-	printf("n\");
+	printf("\n");
 	va_end(strings);
 }
