b73f79e9e99bebfc27f7ae15ca51aa412cba96b1
Zaim211 <yousseftitazaim@gmail.com>
2023-04-27 15:16:40 -0700

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -2,10 +2,10 @@
 void first(void) __attribute__((constructor));
 /**
  * first - function that print sentence before the main
- *
+ * 
  **/
 void first(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
-	printf("I bore my house upon my back!\n);
+	printf("I bore my house upon my back!\n");
 }
