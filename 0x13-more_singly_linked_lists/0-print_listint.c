fd79642c5744e1a5ec76032b530a60113d0bf274
Zaim211 <yousseftitazaim@gmail.com>
2023-04-30 09:25:17 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,13 +8,13 @@
  **/
 size_t print_listint(const listint_t *h)
 {
-	size_t number = 0;
+	size_t n = 0;
 
 	while (h)
 	{
 		printf("%d\n", h->n);
-		number++;
-		h = h->next
+		n++;
+		h = h->next;
 	}
-	return (number);
+	return (n);
 }
