7d110c3bce6b25ab9b898900f3600e0c698f4b6b
Zaim211 <yousseftitazaim@gmail.com>
2023-05-01 09:24:05 -0700

    10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -26,7 +26,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 	while (j < index - 1)
 	{
-		if (!h || !(t->next))
+		if (!h || !(h->next))
 			return (-1);
 		h = h->next;
 		j++;
