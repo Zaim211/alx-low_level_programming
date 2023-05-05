e1d6355b827b1d6f7e65bb22a9f2636a0511e81a
Zaim211 <yousseftitazaim@gmail.com>
2023-05-04 18:30:13 -0700

    5-flip_bits.c

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -7,16 +7,16 @@
  **/
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	int j, h = 0;
-	unsigned long int x;
-	unsigned long int y = n ^ m;
+	int i, count = 0;
+	unsigned long int current;
+	unsigned long int exclusive = n ^ m;
 
-	for (j = 63; j >= 0; j--)
+	for (i = 63; i >= 0; i--)
 	{
-		x = y >> i;
-		if (x & 1)
-			h++;
+		current = exclusive >> i;
+		if (current & 1)
+			count++;
 	}
 
-	return (h);
+	return (count);
 }
