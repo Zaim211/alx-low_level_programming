a214ba115510cb8b58556c07dc57117632b140ce
Zaim211 <yousseftitazaim@gmail.com>
2023-05-10 10:19:16 -0700

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -19,7 +19,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content != NULL)
 	{
-		while (length = 0; text_content[length])
+		for (length = 0; text_content[length];)
 			length++;
 	}
 	fp = open(filename, O_WRONLY | O_APPEND);
