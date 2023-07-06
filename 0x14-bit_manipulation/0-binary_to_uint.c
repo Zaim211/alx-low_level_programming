5b656a0fcdf242b5ffb340af1a45474f6ee5569b
Zaim211 <yousseftitazaim@gmail.com>
2023-07-06 10:10:03 -0700

    task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,11 +11,13 @@ unsigned int binary_to_uint(const char *b)
 
 	if (!b)
 		return (0);
+
 	for (j = 0; b[j]; j++)
 	{
 		if (b[j] < '0' || b[j] > '1')
 			return (0);
-		dec = 2 * dec + (b[j] - '0')
+		dec = 2 * dec + (b[j] - '0');
 	}
+
 	return (dec);
 }
