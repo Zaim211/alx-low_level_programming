c8a31c0ce0eb2704bf70e93e4e741328398a7b9c
Zaim211 <yousseftitazaim@gmail.com>
2023-07-06 10:06:00 -0700

    task 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,7 +8,7 @@ void print_binary(unsigned long int n)
 	int i, count = 0;
 	unsigned long int y;
 
-	for (i + 63; i >= 0; i++)
+	for (i = 63; i >= 0; i--)
 	{
 		y = n >> i;
 		if (y & 1)
@@ -21,6 +21,4 @@ void print_binary(unsigned long int n)
 	}
 	if (!count)
 		_putchar('0');
-
-
 }
