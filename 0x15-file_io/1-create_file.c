63403369dffb8f5c31584af30b8b669b34b7eb25
Zaim211 <yousseftitazaim@gmail.com>
2023-07-10 15:32:57 -0700

    Task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * create_file - function that creates a file
+ * @filename: filename
+ * @text_content: content writed in the file.
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int nmbletters;
+	int n;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY | 0_TRUNC, 0600);
+
+	if (fd == -1)
+		return (-1);
+
+	if (!text_content)
+		text_content = "";
+
+	for (nmbletters = 0; text_content[nmbletters]; nmbletters++)
+	n = write(fd, text_content, nmbletters);
+
+	if (n == -1)
+		return (-1);
+
+	close(fd);
+
+	return (1);
+}
