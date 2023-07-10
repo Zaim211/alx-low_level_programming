2089b10ebe443465b946ac4dab1be4922270a09e
Zaim211 <yousseftitazaim@gmail.com>
2023-07-10 15:51:42 -0700

    Task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,21 +7,26 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, n, length = 0;
+	int fd;
+	int nmbletters;
+	int n;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
 
-	if (text_content != NULL)
-	{
-		for (length = 0; text_content[length];)
-			length++;
-	}
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	n = write(fd, text_content, len);
+	if (fd == -1)
+		return (-1);
 
-	if (fd == -1 || n == -1)
+	if (!text_content)
+		text_content = "";
+
+	for (nmbletters = 0; text_content[nmbletters]; nmbletters++)
+		;
+	n = write(fd, text_content, nmbletters);
+
+	if (n == -1)
 		return (-1);
 
 	close(fd);
