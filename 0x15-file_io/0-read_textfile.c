8538e43ae22d8079fded093b1a512427cba9435d
Zaim211 <yousseftitazaim@gmail.com>
2023-07-10 15:03:02 -0700

    Task 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * read_textfile - function that reads a text file and prints it to POSIX
+ * @filename: filename
+ * @letters: numbers of letters printed
+ * Return: numbers of letters printed, if fails returns 0
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	size_t n, m;
+	char *buf;
+
+	if (!filename)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		return (0);
+
+	buf = malloc(size(char) * (letters));
+	if (!buf)
+		return (0);
+
+	n = read(fd, buf, letters);
+	m = write(STDOUT_FILENO, buf, n);
+
+	close(fd);
+
+	free(buf);
+
+	return (n);
+}
