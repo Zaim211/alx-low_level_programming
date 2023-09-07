da09b002f2306d2df8ea59766dcf0b3f4350d232
Zaim211 <yousseftitzaim@gmail.com>
2023-09-07 13:19:28 -0700

    Task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,13 +14,13 @@ hash_node_t *make_hash_node(const char *key, const char *value)
 	node = malloc(sizeof(hash_node_t));
 	if (node == NULL)
 		return (NULL);
-	node->key = strup(key);
+	node->key = strdup(key);
 	if (node->key == NULL)
 	{
 		free(node);
 		return (NULL);
 	}
-	node->value = strup(value);
+	node->value = strdup(value);
 	if (node->value == NULL)
 	{
 		free(node->key);
