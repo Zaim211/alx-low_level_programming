782be4ebf9849845aa15ae4259670372e7cb1b27
Zaim211 <yousseftitzaim@gmail.com>
2023-09-08 12:18:31 -0700

    Task 5

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -10,12 +10,12 @@ void hash_table_print(const hash_table_t *ht)
 {
 	unsigned long int index;
 	hash_node_t *tmp;
-	char flag;
+	char flag = 0;
 
 	if (ht == NULL || ht->array == NULL)
 		return;
 	printf("{");
-	for (index = 0; ht < size; index++)
+	for (index = 0; index < ht->size; index++)
 	{
 		tmp = ht->array[index];
 		while (tmp != NULL)
